b9a2de7213ae0bc75d50ebc7c2a8137100ecd189
Kanazawanaoaki <kanazawa@jsk.imi.i.u-tokyo.ac.jp>
2022-02-22 13:16:59 +0900

    fix x and y

--- crop-images/crop_img_prosilica.py
+++ crop-images/crop_img_prosilica.py
@@ -14,13 +14,13 @@ h,w,ch = im.shape
 print(h,w)
 
 x_num = int(round(h/32))*9
-# x_size = 720
-x_size = 900
+# x_size = 960
+x_size = 1200
 y_num = int(round(w/32))*3
-# y_size = 960
-y_size = 1200
+# y_size = 720
+y_size = 900
 
-im_cropped = im[x_num:x_num+x_size , y_num:y_num+y_size, :]
+im_cropped = im[y_num:y_num+y_size, x_num:x_num+x_size, :]
 
 cv2.imwrite(path2, im_cropped, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 print(im_cropped.shape)
