095fabce4e75dcf61a20e96272d4c7cec076efb5
Kanazawanaoaki <naoaki65k@gmail.com>
2023-07-16 21:50:31 +0900

    Update movie2image shell script

--- movie2image.bash
+++ movie2image.bash
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for file in `\find . -name '*.mp4'`; do
+movie2image () {
+    file=$1
     after_sra=${file#*/}
     name=${after_sra%%.*}
 	echo "$name"
@@ -10,6 +11,15 @@ for file in `\find . -name '*.mp4'`; do
     else
         mkdir "$mkdir_one_line"
         echo ""$mkdir_one_line"を作りました"
-    fi    
-    ffmpeg -i ${name}.mp4 -ss 0 -t 10 -r 10 -f image2 ${name}/${name}-%06d.jpg
+    fi
+    # ffmpeg -i ${file} -ss 0 -t 10 -r 10 -f image2 ${name}/${name}-%06d.jpg
+    ffmpeg -i ${file} -r 1 -f image2 -vcodec mjpeg -qscale 1 -qmin 1 -qmax 1 ${name}/${name}-%05d.jpg
+}
+
+for file in `\find . -name '*.mp4'`; do
+    movie2image ${file}
+done
+
+for file in `\find . -name '*.MP4'`; do
+    movie2image ${file}
 done
